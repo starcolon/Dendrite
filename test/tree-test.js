29ed4e833ecce8e62dd475e263a019251bba5870
starcolon <pataoengineer@gmail.com>
2015-03-24 02:19:35 +0700

    WRITE TEST CASES FOR BINARY | AVL | REDBLACK BUT PENDING RED BLACK

--- test/tree-test.js
+++ test/tree-test.js
@@ -17,7 +17,6 @@ function remove(v){
 	this.remove(v);
 }
 
-
 //------------------------------------------------------
 describe('@tree checkup starts!', function(){
 
@@ -43,6 +42,39 @@ describe('@tree checkup starts!', function(){
 			done();
 		});
 
+		it ('should not have left branch', function(done){
+			tree.push(120); // Add the right node
+
+			// Check the result
+			assert(tree.isLeftBranchEmpty());
+			done();
+		});
+
+		it ('should NOT auto balance itself after adding multiple values', function(done){
+			var addValue = add.bind(tree);
+			[1,125,124,125,180,300,300,1000].forEach(addValue);
+
+			// Check the result
+			var leftDepth = tree.leftBranch.depth();
+			var rightDepth = tree.rightBranch.depth();
+
+			assert.deepEqual([1,80,120,124,125,125,180,300,300,1000],tree.toKeyArray());
+			assert.equal(tree.key,80); // root must not change
+			assert(Math.abs(leftDepth-rightDepth)>2);
+			done();
+		});
+
+		it ('should NOT auto balance itself after removing multiple values', function(done){
+			var removeValue = remove.bind(tree);
+			[1,125,124,300].forEach(removeValue);
+
+			// left branch should disappear
+			assert(tree.isLeftBranchEmpty());
+
+			// Check the result
+			assert.equal(tree.key,80); // root must not change
+			done();
+		});
 
 	});
 
@@ -67,6 +99,41 @@ describe('@tree checkup starts!', function(){
 			assert.deepEqual([80], tree.toKeyArray());
 			done();
 		});
+
+		it ('should not have left branch', function(done){
+			tree.push(120); // Add the right node
+
+			// Check the result
+			assert(tree.isLeftBranchEmpty());
+			done();
+		});
+
+		it ('should auto balance itself after adding multiple values', function(done){
+			var addValue = add.bind(tree);
+			[1,125,124,125,180,300,300,1000].forEach(addValue);
+
+			// Check the result
+			var leftDepth = tree.leftBranch.depth();
+			var rightDepth = tree.rightBranch.depth();
+
+			assert.deepEqual([1,80,120,124,125,125,180,300,300,1000],tree.toKeyArray());
+			assert.notEqual(tree.key,80); // root must change
+			assert(Math.abs(leftDepth-rightDepth)<2);
+			done();
+		});
+
+		it ('should auto balance itself after removing multiple values', function(done){
+			var removeValue = remove.bind(tree);
+			[1,125,124,300].forEach(removeValue);
+
+			var leftDepth = tree.leftBranch.depth();
+			var rightDepth = tree.rightBranch.depth();
+
+			// Check the result
+			assert.notEqual(tree.key,80); // root must not change
+			assert(Math.abs(leftDepth-rightDepth)<2);
+			done();
+		});
 	});
 
 	describe('@redblacktree checkup starts!==========================', function(){
@@ -88,6 +155,41 @@ describe('@tree checkup starts!', function(){
 
 			// Check the result
 			assert.deepEqual([80], tree.toKeyArray());
+			done();
+		});
+
+		it ('should not have left branch', function(done){
+			tree.push(120); // Add the right node
+
+			// Check the result
+			assert(tree.isLeftBranchEmpty());
+			done();
+		});
+
+		it.skip ('should auto balance itself after adding multiple values', function(done){
+			var addValue = add.bind(tree);
+			[1,125,124,125,180,300,300,1000].forEach(addValue);
+
+			// Check the result
+			var leftDepth = tree.leftBranch.depth();
+			var rightDepth = tree.rightBranch.depth();
+
+			assert.deepEqual([1,80,120,124,125,125,180,300,300,1000],tree.toKeyArray());
+			assert.notEqual(tree.key,80); // root must change
+			assert(Math.abs(leftDepth-rightDepth)<2);
+			done();
+		});
+
+		it.skip ('should auto balance itself after removing multiple values', function(done){
+			var removeValue = remove.bind(tree);
+			[1,125,124,300].forEach(removeValue);
+
+			var leftDepth = tree.leftBranch.depth();
+			var rightDepth = tree.rightBranch.depth();
+
+			// Check the result
+			assert.notEqual(tree.key,80); // root must not change
+			assert(Math.abs(leftDepth-rightDepth)<2);
 			done();
 		});
 	});
