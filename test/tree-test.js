bc8dc10e442387ec290f98161f413fbc6f0ca3a9
starcolon <pataoengineer@gmail.com>
2015-03-25 02:44:24 +0700

    SPLAY OPERATIONS

--- test/tree-test.js
+++ test/tree-test.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 
 var BinaryTree = require('../lib/binarytree.js');
 var AvlTree = require('../lib/avltree.js');
-var RedBlackTree = require('../lib/redblacktree.js');
+var SplayTree = require('../lib/splaytree.js');
 
 //------------------------------------------------------
 // COMMON FUNCTIONS
@@ -167,9 +167,9 @@ describe('@tree checkup starts!', function(){
 		});
 	});
 
-	describe('@redblacktree checkup starts!==========================', function(){
-
-		var tree = new RedBlackTree.RedBlackTree();
+	describe('@splaytree checkup starts!===============================', function(){
+		
+		var tree = new SplayTree.SplayTree();
 
 		it ('should add 3 unordered numbers', function(done){
 			var addValue = add.bind(tree);
@@ -189,39 +189,52 @@ describe('@tree checkup starts!', function(){
 			done();
 		});
 
-		it ('should not have left branch', function(done){
-			tree.push(120); // Add the right node
+		it ('should have new root after add value', function(done){
+			tree.push(120); // Add the right node and it should become the root
 
 			// Check the result
-			assert(tree.isLeftBranchEmpty());
+			assert(tree.isRightBranchEmpty());
+			assert.equal(tree.key,120);
 			done();
 		});
 
-		it.skip ('should auto balance itself after adding multiple values', function(done){
+		it ('should add all nodes and the last added become root', function(done){
+			tree.clear();
 			var addValue = add.bind(tree);
-			[1,125,124,125,180,300,300,1000].forEach(addValue);
+			[1,6,5,3,4].forEach(addValue);
 
 			// Check the result
-			var leftDepth = tree.leftBranch.depth();
-			var rightDepth = tree.rightBranch.depth();
-
-			assert.deepEqual([1,80,120,124,125,125,180,300,300,1000],tree.toKeyArray());
-			assert.notEqual(tree.key,80); // root must change
-			assert(Math.abs(leftDepth-rightDepth)<2);
+			assert.equal(4, tree.key);
+			assert.deepEqual([1,3,4,5,6], tree.toKeyArray());
 			done();
 		});
 
-		it.skip ('should auto balance itself after removing multiple values', function(done){
-			var removeValue = remove.bind(tree);
-			[1,125,124,300].forEach(removeValue);
-
-			var leftDepth = tree.leftBranch.depth();
-			var rightDepth = tree.rightBranch.depth();
+		it ('should add all nodes and the last added become root (bigger tree)', function(done){
+			tree.clear();
+			var addValue = add.bind(tree);
+			[1,6,5,3,4,8,10].forEach(addValue);
 
 			// Check the result
-			assert.notEqual(tree.key,80); // root must not change
-			assert(Math.abs(leftDepth-rightDepth)<2);
+			assert.equal(10, tree.key);
+			assert.deepEqual([1,3,4,5,6,8,10], tree.toKeyArray());
+			done();
+		});
+
+		it ('should add another tree properly', function(done){
+			var anotherTree = new BinaryTree.BinaryTree();
+			tree.clear();
+			var addValue = add.bind(tree);
+			var addValueAnotherTree = add.bind(anotherTree);
+			[1,3,5,7,9].forEach(addValue);
+			[2,4,6].forEach(addValueAnotherTree);
+
+			// Add the tree now
+			tree.add(anotherTree);
+
+			// Check
+			assert.deepEqual([1,2,3,4,5,6,7,9], tree.toKeyArray());
 			done();
 		});
 	});
+	
 });
